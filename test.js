7e4af15c18ed4ab9ae10473d2ab4314342720cb4
Riskoder <g.vidal1g+1@gmail.com>
2024-12-18 16:59:47 -0300

    TEST IN JS IN MERGING FILES

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function testing(n) {
   console.log(n + ` testing git`);
-  return n > 0 || n < 8 ? true : `testing git conflicts`;
+  return n > 0 || n < 8 ? true : `TESTING NUMER 1 OF CONFLICTS`;
 }
 
 console.log(testing(4));
