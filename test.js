7ee79aa5399a329262f24d3a5ec823ba1b0ca745
Riskoder <g.vidal1g+1@gmail.com>
2024-12-18 16:58:40 -0300

    TESTING CONFLICTS 2

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function testing(n) {
-  console.log(n + ` testing git`);
-  return n > 0 || n < 8 ? true : `testing git conflicts`;
+  console.log(`testing`);
+  return `test js`;
 }
 
 console.log(testing(4));
